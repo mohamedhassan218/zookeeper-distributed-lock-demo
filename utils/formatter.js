3260a1c079de1bd294933b9997e8bd46ba9f0677
mohamedhassan218 <m.hassan.def@gmail.com>
2025-10-17 17:38:12 +0300

    Fix the formatter to follow Egypt timezone and test it.

--- utils/formatter.js
+++ utils/formatter.js
@@ -33,7 +33,8 @@ const formatter = new Intl.DateTimeFormat('en-US', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hour12: false
+    hour12: false,
+    timeZone: 'Africa/Cairo'
 });
 
 module.exports = { formatter };
